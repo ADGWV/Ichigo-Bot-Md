696381f4baa2e95c9c1f768e4f62764514e8a42e
Luffy <othmaneboughari@gmail.com>
2024-11-11 15:34:14 +0100

    Update admin.js

--- plugins/admin.js
+++ plugins/admin.js
@@ -1,3 +1,9 @@
+/*
+اسم البوت 𝐈𝐂𝐇𝐈𝐆𝐎-𝐁𝐎𝐓-𝐌𝐃
+تابع حسابي في Github @ADGWV
+اسم الملف  admin.js
+قناه البوت https://whatsapp.com/channel/0029VarBiDU7dmeivAXXgZ0f
+*/
 let handler = async (m, { conn,usedPrefix, text }) => {
 if(isNaN(text) && !text.match(/@/g)){
 
