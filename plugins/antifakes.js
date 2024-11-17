f5c5ad4523b3ac5b7c685f736892f750942ab440
Luffy <othmaneboughari@gmail.com>
2024-11-17 19:23:11 +0100

    Update antifakes.js

--- plugins/antifakes.js
+++ plugins/antifakes.js
@@ -1,3 +1,16 @@
+/*
+
+اسم البوت 𝐈𝐂𝐇𝐈𝐆𝐎-𝐁𝐎𝐓-𝐌𝐃
+
+تابع حسابي في Github @ADGWV
+
+اسم الملف antifakes.js
+
+قناه البوت https://whatsapp.com/channel/0029VarBiDU7dmeivAXXgZ0f
+
+*/
+
+
 var handler = m => m
 handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner }) {
 
